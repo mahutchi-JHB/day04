2334c40bc682686fdf93062ecbb4637969b48b49
Maximilian Hutchison <mahutchi@c4r1s3.wethinkcode.co.za>
2020-07-12 11:39:15 +0200

    ex01 complete

---
+++ ex01/ft_recursive_factor.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int ft_recursive_factorial(int nb){
+	if(nb == 0){
+		return (1);
+	} else if (nb > 12 || nb < 0){
+		return(0);
+	}
+
+	if (nb > 1){
+		return (ft_recursive_factorial(nb - 1) * nb);
+	}
+}
+
+int main(){
+	int nb;
+	nb = 2;
+	
+	return(0)
+}
+
+
+
